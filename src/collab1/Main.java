24a94cb894cf30faff8a15deafdf1f013957ad16
sairaj123456 <sairaj.anvekar@gmail.com>
2015-06-24 16:46:00 +0530

    Update Main.java
    main file

--- src/collab1/Main.java
+++ src/collab1/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Controller cl =new Controller();
+		
+		c1.start();
+		c1.up();
+		c1.up();
+		c1.stop();
+		
+		
+		
+		
 
 	}
 
